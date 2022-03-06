5b3285ba91589f0539193689b772c520759a2674
fawazsullia <fawazsullia@gmail.com>
2022-03-06 17:50:01 +0400

    websocket api for burrow

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const port = process.env.port || 5000
+const { Server } = require("socket.io");
+const fs = require('fs').promises
+
+
+const server = http.createServer(async function(req,res){
+
+if(req.url == "/"){
+    res.end("Websocket server");
+}
+
+if(req.url === "/getmessages"){
+    let msg = await fs.readFile("./messages.txt");
+    res.end(msg);
+}
+
+})
+
+const io = new Server(server, {
+    cors: {
+      origin: "*"
+    }
+  });
+
+io.on("connection",  (socket)=> {
+    socket.on("chat", async (message) => {
+        await fs.appendFile('./messages.txt',message)
+        io.emit("chat", message)
+      })
+})
+
+
+
+
+server.listen(port)
